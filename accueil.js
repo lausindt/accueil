3b216d53ef40b7094632c5f8141e0aa50998db34
Lauriane Sindt <lauiane.sindt@gmail.com>
2017-02-01 10:58:07 +0100

    1st feb - reprise

---
+++ accueil.js
@@ -0,0 +1,21 @@
+function clic() {
+    console.log("Clic !");
+}
+
+var boutonElt = document.getElementById("bouton");
+// Ajout d'un gestionnaire pour l'événement click
+boutonElt.addEventListener("click", clic);
+
+// Gestion de la fin du chargement de la page web
+window.addEventListener("load", function () {
+    console.log("Page entièrement chargée");
+});
+
+// Gestion de la fermeture de la page web
+window.addEventListener("beforeunload", function (e) {
+    var message = "On est bien ici !";
+    e.returnValue = message; // Provoque une demande de confirmation (standard)
+    return message; // Provoque une demande de confirmation (certains navigateurs)
+});
+// Focus sur la zone de saisie du pseudo
+pseudoElt.focus();
