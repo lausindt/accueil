7c1ce76740b824a1cb79c6a8b7c27f381b9b5e60
Lauriane Sindt <lauiane.sindt@gmail.com>
2017-02-16 15:30:09 +0100

    boutons détail

--- accueil.js
+++ accueil.js
@@ -19,55 +19,40 @@ $(function() {
     }
         
 });
+  
     
+      
     $('#affiche-a2016').click(function() {
-        $('#a2016').show('slow');
-    } ); 
-    $('#cache-a2016').click(function() {
-        $('#a2016').hide(1000);
-    });
+        if($('#a2016').is(':hidden')){$('#a2016').show('slow');}
+        else{$('#a2016').hide(1000);}
+        });
     $('#affiche-a2015-2').click(function() {
-        $('#a2015-2').show('slow');
-    } ); 
-    $('#cache-a2015-2').click(function() {
-        $('#a2015-2').hide(1000);
-    });
+        if($('#a2015-2').is(':hidden')){$('#a2015-2').show('slow');}
+        else{$('#a2015-2').hide(1000);}
+        });
     $('#affiche-a2015-1').click(function() {
-        $('#a2015-1').show('slow');
-    } ); 
-    $('#cache-a2015-1').click(function() {
-        $('#a2015-1').hide(1000);
-    });
+        if($('#a2015-1').is(':hidden')){$('#a2015-1').show('slow');}
+        else{$('#a2015-1').hide(1000);}
+        });
     $('#affiche-lcms').click(function() {
-        $('#lcms').show('slow');
-    } ); 
-    $('#cache-lcms').click(function() {
-        $('#lcms').hide(1000);
-    });
-    
+        if($('#lcms').is(':hidden')){$('#lcms').show('slow');}
+        else{$('#lcms').hide(1000);}
+        });
     $('#affiche-rmn').click(function() {
-        $('#rmn').show('slow');
-    } ); 
-    $('#cache-rmn').click(function() {
-        $('#rmn').hide(1000);
-    });
-    
+        if($('#rmn').is(':hidden')){$('#rmn').show('slow');}
+        else{$('#rmn').hide(1000);}
+        });
     $('#affiche-oak').click(function() {
-        $('#oak').show('slow');
-    } ); 
-    $('#cache-oak').click(function() {
-        $('#oak').hide(1000);
-    });
+        if($('#oak').is(':hidden')){$('#oak').show('slow');}
+        else{$('#oak').hide(1000);}
+        });
     $('#affiche-cpc').click(function() {
-        $('#cpc').show('slow');
-    } ); 
-    $('#cache-cpc').click(function() {
-        $('#cpc').hide(1000);
-    });
+        if($('#cpc').is(':hidden')){$('#cpc').show('slow');}
+        else{$('#cpc').hide(1000);}
+        });
     $('#affiche-lc').click(function() {
-        $('#lc').show('slow');
-    } ); 
-    $('#cache-lc').click(function() {
-        $('#lc').hide(1000);
-    });
+        if($('#lc').is(':hidden')){$('#lc').show('slow');}
+        else{$('#lc').hide(1000);}
+        });
+    
 });
